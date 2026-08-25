05a9fe1e49ff29c4f149a99033a33dcdc209d970
vuledev <77647824+mystixxx@users.noreply.github.com>
2026-08-25 02:21:19 +0200

    Optimize RunPod container startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-# 1) Remove any unwanted OpenCV desktop build
-pip uninstall -y opencv-python   || true
-
-# 2) Make absolutely sure the headless wheel is in use
-pip install --no-deps --force-reinstall opencv-python-headless
-
 exec python3 src/handler.py "$@"
